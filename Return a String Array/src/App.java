a05e81cb1608185c5589f3a359c81cc94352ea93
Samer Khamisi <skhamisi@outlook.com>
2022-01-28 23:54:10 -0600

    '

--- Return a String Array/src/App.java
+++ Return a String Array/src/App.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        ClearScreen();
+
         try {
         Scanner scan = new Scanner(System.in);
         System.out.print("Please enter 3 values seperated by a comma: ");
         String input = scan.nextLine();
 
         String values[] = returnArray(input);
+        System.out.print("\n");
 
         for (String i : values) {
-            System.out.println("\n" + i);
+            System.out.println(i);
         }
 
         scan.close();
@@ -25,5 +28,11 @@ public class App {
 
         String[] userValues = input.split(", ");
         return userValues;
+    }
+
+    public static void ClearScreen() 
+    {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
     }
 }
