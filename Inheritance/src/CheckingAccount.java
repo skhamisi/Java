5e7b9ff1645d946becf55d18eda0947751fea34d
Samer Khamisi <samer.n.khamisi@outlook.com>
2022-02-16 21:11:20 -0600

    Extracted CalculateInterest() from Display()

--- Inheritance/src/CheckingAccount.java
+++ Inheritance/src/CheckingAccount.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Collections;
 
 class CheckingAccount extends BankAccount {
     static Random rn = new Random();
@@ -23,23 +26,53 @@ class CheckingAccount extends BankAccount {
         }
     }
 
-    void DisplayAccount() {
+    double[] CalculateInterest() {
         double apy = .01;
         double interestEarned;
         double newBalance;
+        // List<Double> values = new ArrayList<>();
+        double[] values = new double[2];
 
-        if (balance > 0) {
+        if (balance > 0 && balance <= 10000) {
             interestEarned = balance * apy;
             newBalance = balance + interestEarned;
             setBalance(newBalance);
+        }
+        else if (balance > 1000) {
+            apy = .025;
+            interestEarned = balance * apy;
         }
         else {
             interestEarned = 0;
         }
 
+        values[0] = interestEarned;
+        values[1] = apy;
+        // values.add(interestEarned);
+        // values.add(apy);
+        return values;
+    }
+
+    void DisplayAccount() {
+        // double apy = .01;
+        // double interestEarned;
+        // double newBalance;
+
+        // if (balance > 0 && balance <= 10000) {
+        //     interestEarned = balance * apy;
+        //     newBalance = balance + interestEarned;
+        //     setBalance(newBalance);
+        // }
+        // else if (balance > 1000) {
+
+        // }
+        // else {
+        //     interestEarned = 0;
+        // }
+        double[] values = CalculateInterest(); 
         AccountSummary();
-        System.out.println("APY: " + apy + "%");
-        System.out.println("Interest Earned: $" + interestEarned);
+        System.out.println("APY: " + values[1] + "%");
+        System.out.println("Interest Earned: $" + values[0]);
     }
 
     static CheckingAccount CreateAccount() {
@@ -52,5 +85,10 @@ class CheckingAccount extends BankAccount {
 
         CheckingAccount newAccount = new CheckingAccount(firstName, lastName, accountID, 0);
         return newAccount;
+    }
+
+    static void clearScreen() {  
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
     }
 }
--- Inheritance/src/Main.java
+++ Inheritance/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        CheckingAccount.clearScreen();
         CheckingAccount account1 = CheckingAccount.CreateAccount();
 
         System.out.print("\nHow much would you like to deposit?: $");
